f0ae2a8affb27a10e4b036b1e320790a64539b98
franklinT <franklintsung3@gmail.com>
2022-05-24 21:23:19 -0400

    update

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: 'localhost', 
     user: 'root', 
-    password: 'Ext187Mv#3', 
+    password: '', 
     database: 'employees'
 }, 
 console.log('Connected to the election database!')); 
